8f3a29588db1d12c8d0c128d15504984fb09c89d
Agus Suandi <agussuandi48@gmail.com>
2023-03-03 14:22:38 +0700

    fixed

--- backend/app/Http/Resources/CustomerReceiptResource.php
+++ backend/app/Http/Resources/CustomerReceiptResource.php
@@ -14,22 +14,22 @@ class CustomerReceiptResource extends JsonResource
     public function toArray($request): array
     {
         return [
-            'customerReceiptId'              => $this->id,
-            'customerReceiptCode'            => $this->code,
-            'customerReceiptName'            => $this->name,
-            'customerId'                     => $this->customer_id,
-            'customerName'                   => $this->customer->name,
-            'customerCode'                   => $this->customer->code,
-            'customerReceiptSpherisRight'    => $this->spheris_right,
-            'customerReceiptSpherisLeft'     => $this->spheris_left,
-            'customerReceiptCylinderRight'   => $this->cylinder_right,
-            'customerReceiptCylinderLeft'    => $this->cylinder_left,
-            'customerReceiptAdditionRight'   => $this->addition_right,
-            'customerReceiptAdditionLeft'    => $this->addition_left,
-            'customerReceiptAxisRight'       => $this->axis_right,
-            'customerReceiptAxisLeft'        => $this->axis_left,
-            'customerReceiptCreatedAt'       => $this->created_at,
-            'customerReceiptUpdatedAt'       => $this->updated_at,
+            'customerReceiptId'            => $this->id,
+            'customerReceiptCode'          => $this->code,
+            'customerReceiptName'          => $this->name,
+            'customerId'                   => $this->customer_id,
+            'customerName'                 => $this->customer->name,
+            'customerCode'                 => $this->customer->code,
+            'customerReceiptSpherisRight'  => $this->spheris_right,
+            'customerReceiptSpherisLeft'   => $this->spheris_left,
+            'customerReceiptCylinderRight' => $this->cylinder_right,
+            'customerReceiptCylinderLeft'  => $this->cylinder_left,
+            'customerReceiptAdditionRight' => $this->addition_right,
+            'customerReceiptAdditionLeft'  => $this->addition_left,
+            'customerReceiptAxisRight'     => $this->axis_right,
+            'customerReceiptAxisLeft'      => $this->axis_left,
+            'customerReceiptCreatedAt'     => $this->created_at,
+            'customerReceiptUpdatedAt'     => $this->updated_at,
         ];
     }
 }
