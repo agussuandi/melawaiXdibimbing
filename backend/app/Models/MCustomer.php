e10ea99d2811c2cb0a4c4a2f8b2bdd9edaf2e0e8
Agus Suandi <agussuandi48@gmail.com>
2023-03-06 13:43:21 +0700

    add relation customer to customer receipts

--- backend/app/Models/MCustomer.php
+++ backend/app/Models/MCustomer.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class MCustomer extends Model
 {
@@ -14,4 +15,9 @@ class MCustomer extends Model
     public $timestamps = false;
     
     protected $guarded = ['id'];
+
+    public function customerReceipts(): HasMany
+    {
+        return $this->hasMany(MCustomerReceipt::class, 'customer_id', 'id');
+    }
 }
